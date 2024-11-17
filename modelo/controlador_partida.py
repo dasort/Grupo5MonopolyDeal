bad289fe3cb30f24b36bcf04d21dbfe2c1c72a9f
Vidal Diego <141795035+VidalDiegoo@users.noreply.github.com>
2024-11-16 22:14:17 -0300

    Update controlador_partida.py
    Necesitan cambios que estoy implementando

--- modelo/controlador_partida.py
+++ modelo/controlador_partida.py
@@ -1,1 +1,51 @@
-
+from jugador import Jugador
+import random
+from carta import Carta
+class Controlador:
+    def __init__(self, jugadores: Jugador):
+        self.__jugadores = jugadores  # Instancias de la clase Jugador
+        self.__mazo = []
+        self.__mesa = None
+        self.__cartas_descarte = []  # Pila de descarte
+        self.__turno_actual = 0  
+        self.repartir_cartas()
+    def repartir_cartas(self):
+        for jugador in self.__jugadores:
+            cartas_a_repartir = self.sacar_cartas(5)  # Obtiene una lista de 5 cartas
+            for carta in cartas_a_repartir:
+                jugador.tomar_carta(carta)  # Añade cada carta individualmente
+    def tomar_carta_mazo(self):
+        carta = random.choice(self.__mazo)
+        self.__mazo.remove(carta)
+        return carta
+    # Otra funcion que necesita cambios cuando se termine de hacer el mazo
+    def jugar_carta(self, jugador: Jugador):
+        carta = self.elijir_carta(jugador)
+        # Verificar si la carta puede ser jugada
+        if carta.tipo == "Accion":
+                self.ejecutar_accion(carta)
+                self.__mesa = carta
+        elif carta.tipo == "Propiedad":
+            jugador.agregar_a_propiedad(carta)
+            print("Se agrego una propiedad")
+        elif carta.tipo == "Dinero":
+            jugador.agregar_a_banco(carta)
+            print("Se agrego al banco")
+            
+    def ejecutar_accion(self, carta):
+        print("Ejecutando Accion: ")
+        carta.accion(self.__jugadores)
+    # Elije la una carta de la mano esta funcion necesita cambios pero es la idea de lo que hay que hacer
+    def  elijir_carta(self,jugador: Jugador) -> Carta:
+        print("Elije la Carta")
+        jugador.mostrar_mano()
+        # Implementacion sin interfaz
+        carta_id = int(input("Dame el ID de la carta a Jugar: "))
+        return jugador.jugar_carta(carta_id)
+    
+    # Saca una cantidad esacta de cartas del mazo
+    def sacar_cartas(self, cantidad: int):
+        #self.__mazo = random.shuffle(self.__mazo)
+        cartas = self.__mazo[:cantidad]
+        self.__mazo = self.__mazo[cantidad:]
+        return cartas
