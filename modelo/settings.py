b6b51c163b92e9176a93b97a19954fb25286bd59
Dahiana Williams <73578746+dahiwms@users.noreply.github.com>
2024-11-17 01:11:49 -0300

    settings de todas las cartas
    (no terminado)
    por agregar: - los comodines en carta.py 
    -  seguir modificando settings.py

---
+++ modelo/settings.py
@@ -0,0 +1,58 @@
+#tiene toda la config de las cartas - nota: faltan los comodines en carta.py y modificar los settings
+
+#propiedades 28 cartas  2 marron,2 azul, 3 celestes
+LISTA_PROPIEDADES= [
+    {"id": 1, "descripcion": "Mediterranean Avenue", "tipo": "propiedad", "valor": 1, "color": "marron"},
+    {"id": 2, "descripcion": "BALTIC AVENUE", "tipo": "propiedad", "valor": 1, "color": "marron"},
+    {"id": 3, "descripcion": "Reading Railroad", "tipo": "propiedad", "valor": 2, "color": "ferrocarril"},
+    {"id": 4, "descripcion": "Oriental Avenue", "tipo": "propiedad", "valor": 2, "color": "celeste"},
+    {"id": 5, "descripcion": "Vermont Avenue", "tipo": "propiedad", "valor": 2, "color": "celeste"},
+    {"id": 6, "descripcion": "Connecticut Avenue", "tipo": "propiedad", "valor": 3, "color": "celeste"},
+    {"id": 7, "descripcion": "St. Charles Place", "tipo": "propiedad", "valor": 3, "color": "rosa"},
+    {"id": 8, "descripcion": "Electric Company", "tipo": "propiedad", "valor": 3, "color": "servicio"},
+    {"id": 9, "descripcion": "States Avenue", "tipo": "propiedad", "valor": 4, "color": "rosa"},
+    {"id": 10, "descripcion": "Virginia Avenue", "tipo": "propiedad", "valor": 4, "color": "rosa"},
+    {"id": 11, "descripcion": "St. James Place", "tipo": "propiedad", "valor": 4, "color": "naranja"},
+    {"id": 12, "descripcion": "Tennessee Avenue", "tipo": "propiedad", "valor": 5, "color": "naranja"},
+    {"id": 13, "descripcion": "New York Avenue", "tipo": "propiedad", "valor": 5, "color": "naranja"},
+    {"id": 14, "descripcion": "Kentucky Avenue", "tipo": "propiedad", "valor": 6, "color": "rojo"},
+    {"id": 15, "descripcion": "Chance", "tipo": "accion", "valor": 0, "color": "accion"},
+    {"id": 16, "descripcion": "Indiana Avenue", "tipo": "propiedad", "valor": 6, "color": "rojo"},
+    {"id": 17, "descripcion": "Illinois Avenue", "tipo": "propiedad", "valor": 6, "color": "rojo"},
+    {"id": 18, "descripcion": "B&O Railroad", "tipo": "propiedad", "valor": 2, "color": "ferrocarril"},
+    {"id": 19, "descripcion": "Atlantic Avenue", "tipo": "propiedad", "valor": 7, "color": "amarillo"},
+    {"id": 20, "descripcion": "Ventnor Avenue", "tipo": "propiedad", "valor": 7, "color": "amarillo"},
+    {"id": 21, "descripcion": "Water Works", "tipo": "propiedad", "valor": 3, "color": "servicio"},
+    {"id": 22, "descripcion": "Marvin Gardens", "tipo": "propiedad", "valor": 7, "color": "amarillo"},
+    
+    {"id": 23, "descripcion": "Pacific Avenue", "tipo": "propiedad", "valor": 8, "color": "verde"},
+    {"id": 24, "descripcion": "North Carolina Avenue", "tipo": "propiedad", "valor": 8, "color": "verde"},
+    
+    {"id": 25, "descripcion": "Pennsylvania Railroad", "tipo": "propiedad", "valor": 2, "color": "ferrocarril"},
+    {"id": 26, "descripcion": "Park Place", "tipo": "propiedad", "valor": 9, "color": "azul"},
+    {"id": 27, "descripcion": "Boardwalk", "tipo": "propiedad", "valor": 10, "color": "azul"},
+    {"id": 28, "descripcion": "Penssylvania Avenue", "tipo": "propiedad", "valor": 8, "color": "verde"}
+]
+
+
+LISTA_DINERO= [
+    {"id": 1, "descripcion": "Dinero de 1", "tipo": "dinero"},
+    {"id": 2, "descripcion": "Dinero de 2", "tipo": "dinero"},
+    {"id": 3, "descripcion": "Dinero de 3", "tipo": "dinero"},
+    {"id": 4, "descripcion": "Dinero de 4", "tipo": "dinero"},
+    {"id": 5, "descripcion": "Dinero de 5", "tipo": "dinero"},
+    {"id": 6, "descripcion": "Dinero de 6", "tipo": "dinero"},
+    {"id": 7, "descripcion": "Dinero de 7", "tipo": "dinero"},
+]
+
+# Datos de las cartas de acción de Monopoly Deal
+LISTA_ACCIONES= [
+    {"id": 1, "descripcion": "Roba Propiedad", "tipo": "accion", "accion": #aca irian las acciones},
+    {"id": 2, "descripcion": "Paga la Renta", "tipo": "accion", "accion": #aca irian las acciones}
+    {"id": 3, "descripcion": "Roba Dinero", "tipo": "accion", "accion":#aca irian las acciones},
+    {"id": 4, "descripcion": "Cambia Propiedad", "tipo": "accion", "accion": #aca irian las acciones},
+    {"id": 5, "descripcion": "Saca una Carta", "tipo": "accion", "accion": #aca irian las acciones},
+    {"id": 6, "descripcion": "Termina el Turno", "tipo": "accion", "accion": },
+    {"id": 7, "descripcion": "Deja de Pagar", "tipo": "accion", "accion": },
+    {"id": 8, "descripcion": "Roba una Carta de Renta", "tipo": "accion", "accion": },
+]
