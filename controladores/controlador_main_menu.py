ce4f7eb5ada0c23722e1515b9865b82aa1d65509
Lucas Bonifacio <lucasub360@gmail.com>
2024-11-25 03:13:43 -0300

    Varios cambios importantes
    Ahora ya no saltan más los errores de los pixmaps "QPixmap::scaled: Pixmap is a null pixmap".

--- controladores/controlador_main_menu.py
+++ controladores/controlador_main_menu.py
@@ -6,45 +6,42 @@ from vistas.vista_como_juego import ComoJuego
 from vistas.vista_opciones import Opciones
 from vistas.vista_estadisticas import Estadisticas
 
-
 class controlador_main_menu (QMainWindow):
     def __init__(self): 
         super().__init__()
         
-        
         self.player = QMediaPlayer()
         
-        #funciones de los botones
+        # Funciones de los botones:
         self.boton_crear_partida=CrearPartida(self)
         self.boton_opciones=Opciones(self)
         self.boton_como_juego=ComoJuego(self)
         self.boton_estadisticas=Estadisticas(self)
     
-    #sonido de botones
+    # Sonido de los botones:
     def sonido_click(self):
         self.player.stop()
         self.player.play()
     
-    #boton crear partida    
+    # Botón crear partida:
     def mostrar_crear_partida(self):
         self.sonido_click()
         self.hide()
         self.boton_crear_partida.exec()
     
-    #boton opciones:
-    
+    # Botón opciones:
     def mostrar_opciones(self):
         self.sonido_click()
         self.hide()
         self.boton_opciones.exec()
         
-    #boton como jugar:
+    # Botón cómo jugar:
     def mostrar_como_juego(self):
         self.sonido_click()
         self.hide()
         self.boton_como_juego.exec()
     
-    #boton estadisticas:
+    # Botón estadísticas:
     def mostrar_estadisticas_inicio_sesion(self):
         self.sonido_click()
         self.hide()
