bff56cc62dd1cab368c56faf96c4ff660a55a82a
Lucas Bonifacio <lucasub360@gmail.com>
2024-11-25 01:17:41 -0300

    Hice el iniciar_sesion.py, y ahora se ve un ojo en el echo mode.
    Le agregué también el título de Monopoly Deal que me pidió carlitos, falta nomás que le haga el controlador, y le de funcionalidad con la base de datos. (Y ver donde ponerlo también).

--- como_juego.py
+++ como_juego.py
@@ -24,7 +24,7 @@
 
 
 from PyQt6.QtWidgets import QDialog, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QSpacerItem, QSizePolicy, QFrame, QMessageBox
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QPixmap
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QGuiApplication
 
@@ -36,87 +36,168 @@ class ComoJuego(QDialog):
         self.setGeometry(340, 130, 900, 600)
         self.setWindowIcon(QIcon("imagenes/ui/icono.png"))
         self.centrar_ventana()
-        
-
-        # ---
-
-        self.layout = QVBoxLayout()
-        self.layout_para_centrar_botones = QVBoxLayout()
 
         # ---
         
-        self.layout.addSpacing(10)
-        
-        # Título:
-        self.label = QLabel("Selecciona que quieres explorar:", self)
-        self.layout.addWidget(self.label)
-        
-        self.layout.addSpacing(20)
-        
-        # Literalmente una línea:
-        linea1 = QFrame(self)
-        linea1.setFrameShape(QFrame.Shape.HLine)
-        linea1.setFrameShadow(QFrame.Shadow.Sunken)
-        self.layout.addWidget(linea1)
-        
-        self.layout.addSpacing(40)
-        
-        # Separador:
-        # self.layout.addSpacerItem(QSpacerItem(20, 400, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed))
+        # Main layout:
+        self.main_layout = QVBoxLayout()
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+        self.setLayout(self.main_layout)
 
-        # Boton a:
-        boton_a = QPushButton("Reglas generales.", self)
-        boton_a.setFixedWidth(250)
+        # Widgets:
+        self.widget_arriba = QWidget()
+        self.widget_abajo = QWidget()
         
-        # Boton b:
-        boton_b = QPushButton("Que hacen las cartas.", self)
-        boton_b.setFixedWidth(250)
+        # Estilos de los widgets:
+        #self.widget_arriba.setStyleSheet("""
+        #    background-color: rgba(89, 45, 22, 1);
+        #    border-bottom: 3px solid rgba(72, 26, 11, 1);
+        #""")
+        #self.widget_abajo.setStyleSheet("""
+        #    background-color: rgba(89, 45, 22, 1);
+        #""")
         
-        # Boton c:
-        boton_c = QPushButton("Sobre la interfaz.", self)
-        boton_c.setFixedWidth(250)
+        # Layouts (Les paso los Widgets):
+        self.layout_arriba = QVBoxLayout(self.widget_arriba)
+        self.layout_arriba.setContentsMargins(0, 0, 0, 0)
+        self.layout_arriba.setSpacing(0)
         
-        self.layout_para_centrar_botones.addWidget(boton_a, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.layout_para_centrar_botones.addSpacing(2)
-        self.layout_para_centrar_botones.addWidget(boton_b, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.layout_para_centrar_botones.addSpacing(2)
-        self.layout_para_centrar_botones.addWidget(boton_c, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.layout.addLayout(self.layout_para_centrar_botones)
+        self.layout_arriba_arriba = QHBoxLayout()
+        self.layout_arriba_arriba.setContentsMargins(10, 10, 0, 0)
+        self.layout_arriba_arriba.setSpacing(0)
+        self.layout_arriba_arriba.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         
-        self.layout.addSpacing(40)
+        self.layout_arriba_abajo = QHBoxLayout()
+        self.layout_arriba_abajo.setContentsMargins(20, 5, 20, 10)
+        self.layout_arriba_abajo.setSpacing(12)
+        self.layout_arriba_abajo.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
         
-        # Otra línea:
-        linea1 = QFrame(self)
-        linea1.setFrameShape(QFrame.Shape.HLine)
-        linea1.setFrameShadow(QFrame.Shadow.Sunken)
-        self.layout.addWidget(linea1)
+        # El layout_abajo cambia dinámicamente.
+        #self.mostrar_reglas_generales() # <-- Acá me aseguro que empiece con la de Reglas Generales como predeterminado.
         
-        self.layout.addSpacing(10)
+        # ---
         
         # Volver:
-        self.boton_volver = QPushButton("Volver al Menú Principal", self)
+        self.boton_volver = self.crear_boton_volver("Volver", "imagenes/ui/home.png")
         self.boton_volver.clicked.connect(self.volver)
-        self.layout.addWidget(self.boton_volver)
+        self.layout_arriba_arriba.addWidget(self.boton_volver)
         
-        self.setLayout(self.layout)
+        # ---
         
-    def mostrar_creditos(self):
-        mensaje = QMessageBox(self)
-        mensaje.setWindowTitle("Créditos")
-        mensaje.setText("Nosotros desarrollamos este juego:")
-        mensaje.setInformativeText(
-            "\n\nBonifacio, Lucas\n"
-            "Cárdenas, Franco\n"
-            "Lopes, Carlos\n"
-            "Gonzales, Nadin\n"
-            "Cabana, Ricardo (Abandonó)\n"
-            "Williams, Dahiana\n"
-            "Vidal, Maida Diego\n"
-            "Ampuero, Alejandro\n"
-            "Contreras, Joel\n")
-        mensaje.setIcon(QMessageBox.Icon.Information)
-        mensaje.setStandardButtons(QMessageBox.StandardButton.Ignore)
-        mensaje.exec()
+        # Botón "Reglas Generales":
+        self.boton_reglas_generales = self.crear_boton("Reglas Generales", "imagenes/ui/book_question.png")
+        self.boton_reglas_generales.clicked.connect(self.mostrar_reglas_generales)
+        self.layout_arriba_abajo.addWidget(self.boton_reglas_generales)
+        
+        # Botón "Ver las Cartas":
+        self.boton_ver_cartas = self.crear_boton("Ver las Cartas", "imagenes/ui/blogs_stack.png")
+        self.boton_ver_cartas.clicked.connect(self.mostrar_ver_cartas)
+        self.layout_arriba_abajo.addWidget(self.boton_ver_cartas)
+        
+        # Botón "A cerca de la Interfaz":
+        self.boton_a_cerca_interfaz = self.crear_boton("A cerca de la Interfaz", "imagenes/ui/checkerboard.png")
+        self.boton_a_cerca_interfaz.clicked.connect(self.mostrar_a_cerca_interfaz)
+        self.layout_arriba_abajo.addWidget(self.boton_a_cerca_interfaz)
+
+        # ---
+        
+        self.layout_arriba.addLayout(self.layout_arriba_arriba)
+        self.layout_arriba.addLayout(self.layout_arriba_abajo)
+        
+        self.main_layout.addWidget(self.widget_arriba)
+        #self.main_layout.addWidget(self.layout_arriba_abajo) <-- No porque es dinámico.
+    
+    def crear_boton(self, texto, icono_ruta):
+        """Crea un botón que tiene un ícono (a la izquierda) y texto."""
+        boton = QPushButton(self) # <-- Este es el contenedor principal.
+        boton.setStyleSheet("""
+            QPushButton {
+                border: 2px solid #444;
+                border-radius: 5px;
+                background-color: #222; /* <-- Es perfecto no cambiar */
+                font-size: 18px;
+                text-align: left;
+            }
+            QPushButton:hover {
+                background-color: #333; /* <-- Tampoco */
+            }
+        """)
+        boton.setFixedHeight(50)
+        boton.setFixedWidth(250)
+
+        # Layout del ícono y el texto:
+        layout = QHBoxLayout(boton)
+
+        # Ícono
+        icono_label = QLabel(self)
+        pixmap = QPixmap(icono_ruta).scaled(20, 20) # <-- Tamaño de la imagen.
+        icono_label.setPixmap(pixmap)
+        icono_label.setFixedSize(20, 20)            # <-- Tamaño fijo del contenedor del ícono.
+        layout.addWidget(icono_label)
+
+        # Texto
+        texto_label = QLabel(texto, self)
+        texto_label.setStyleSheet("""
+            padding-left: 5px;
+            font-size: 18px;
+        """)
+        layout.addWidget(texto_label)
+
+        return boton
+    
+    def crear_boton_volver(self, texto, icono_ruta):
+        """Crea un botón que tiene un ícono (a la izquierda) y texto."""
+        boton = QPushButton(self) # <-- Este es el contenedor principal.
+        boton.setStyleSheet("""
+            QPushButton {
+                border: 2px solid #444;
+                border-radius: 3px;
+                background-color: #222; /* <-- Es perfecto no cambiar */
+                font-size: 10px;
+                text-align: left;
+            }
+            QPushButton:hover {
+                background-color: #333; /* <-- Tampoco */
+            }
+        """)
+        boton.setFixedHeight(30)
+        boton.setFixedWidth(175)
+
+        # Layout del ícono y el texto:
+        layout = QHBoxLayout(boton)
+
+        # Ícono
+        icono_label = QLabel(self)
+        pixmap = QPixmap(icono_ruta).scaled(15, 15) # <-- Tamaño de la imagen.
+        icono_label.setPixmap(pixmap)
+        icono_label.setFixedSize(15, 15)            # <-- Tamaño fijo del contenedor del ícono.
+        layout.addWidget(icono_label)
+
+        # Texto
+        texto_label = QLabel(texto, self)
+        texto_label.setStyleSheet("""
+            padding-left: 5px;
+            font-size: 16px;
+        """)
+        layout.addWidget(texto_label)
+
+        return boton
+    
+    def mostrar_reglas_generales(self):
+        pass
+    
+        # Una línea:
+        linea = QFrame(self)
+        linea.setFrameShape(QFrame.Shape.HLine)
+        linea.setFrameShadow(QFrame.Shadow.Sunken)
+        self.layout.addWidget(linea)
+        
+    def mostrar_ver_cartas(self):
+        pass
+        
+    def mostrar_a_cerca_interfaz(self):
+        pass
 
     def volver(self):
         self.hide()
--- iniciar_sesion.py
+++ iniciar_sesion.py
@@ -1,8 +1,8 @@
 import sys
 import psycopg2
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QFrame
-from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QFrame, QSpacerItem, QSizePolicy
+from PyQt6.QtGui import QIcon, QPixmap, QGuiApplication
+from PyQt6.QtCore import Qt, QTimer
 from crear_partida import CrearPartida
 
 class IniciarSesion(QDialog):
@@ -12,6 +12,7 @@ class IniciarSesion(QDialog):
         self.setWindowTitle("Iniciar Sesión")
         self.setGeometry(480, 200, 600, 450)
         self.setWindowIcon(QIcon("imagenes/ui/icono.png"))
+        self.centrar_ventana()
         
         self.main_layout = QVBoxLayout(self)
         self.setLayout(self.main_layout)
@@ -19,17 +20,89 @@ class IniciarSesion(QDialog):
         #self.setMinimumSize(600, 300)
         
         # ---
+        
+        # Imagen encabezado:
+        self.imagen_label = QLabel(self)
+        pixmap = QPixmap("imagenes/ui/headerLogo.png").scaled(
+            400, 100, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation
+        )
+        self.imagen_label.setPixmap(pixmap)
+        self.imagen_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # ---
 
+        # Label "Nombre de Usuario":
         self.username_label = QLabel("Nombre de Usuario:")
+        self.username_label.setStyleSheet("""
+            /*padding: 6px;*/
+            font-size: 20px;
+        """)
+        
+        # LineEdit del nombre de usuario:
         self.username_input = QLineEdit(self)
         self.username_input.setPlaceholderText("Ingrese su nombre de usuario.")
+        self.username_input.setFixedHeight(33)
+        self.username_input.setStyleSheet("""
+            font-size: 20px;
+        """)
         
         # ---
         
+        # Label "Contraseña":
         self.password_label = QLabel("Contraseña:")
+        self.password_label.setStyleSheet("""
+            /*padding: 6px;*/
+            font-size: 20px;
+        """)
+        
+        # Layout horizontal de la contraseña:
+        
+        self.layout_contrasenia = QHBoxLayout()
+        
+        # (1/2) LineEdit de la contraseña:
         self.password_input = QLineEdit(self)
         self.password_input.setPlaceholderText("Ingrese su contraseña.")
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password_input.setFixedHeight(33)
+        self.password_input.setStyleSheet("""
+            font-size: 20px;
+        """)
+        self.layout_contrasenia.addWidget(self.password_input) # <-- Agrego 1/2 (El LineEdit).
+        
+        # (2/2) Botón de la contraseña:
+        self.boton_ojo = QPushButton(self) # <-- Este es el contenedor principal.
+        self.boton_ojo.setStyleSheet("""
+            QPushButton {
+                border: 2px solid #444;
+                border-radius: 3px;
+                background-color: #222; /* <-- Es perfecto no cambiar */
+                font-size: 10px;
+                text-align: left;
+            }
+            QPushButton:hover {
+                background-color: #333; /* <-- Tampoco */
+            }
+        """)
+        self.boton_ojo.clicked.connect(self.alternar_modo_echo)
+        self.boton_ojo.setFixedHeight(30)
+        self.boton_ojo.setFixedWidth(60)
+        tooltip_text = (
+            "Es realmente muy recomendable que<br>"
+            "priorice su privacidad."
+        )
+        self.boton_ojo.setToolTip(tooltip_text)
+
+        # Layout del ícono (y el texto por si queremos agregarle "Ver contraseña"):
+        self.layout_interno_ojo = QHBoxLayout(self.boton_ojo)
+
+        # Ícono:
+        self.icono_label = QLabel(self)
+        pixmap = QPixmap("imagenes/ui/ojo_cerrado.png").scaled(25, 25)
+        self.icono_label.setPixmap(pixmap)
+        self.icono_label.setFixedSize(25, 15)
+        self.layout_interno_ojo.addWidget(self.icono_label)
+        
+        self.layout_contrasenia.addWidget(self.boton_ojo) # <-- Agrego 2/2 (El botón).
         
         # ---
         
@@ -74,24 +147,29 @@ class IniciarSesion(QDialog):
         """)
         self.boton_volver.clicked.connect(self.volver)
         
-        linea1 = QFrame(self)
-        linea2 = QFrame(self)
-        linea1.setFrameShape(QFrame.Shape.HLine)
-        linea2.setFrameShape(QFrame.Shape.HLine)
-        linea1.setFrameShadow(QFrame.Shadow.Sunken)
-        linea2.setFrameShadow(QFrame.Shadow.Sunken)
+        #linea1 = QFrame(self)
+        #linea2 = QFrame(self)
+        #linea1.setFrameShape(QFrame.Shape.HLine)
+        #linea2.setFrameShape(QFrame.Shape.HLine)
+        #linea1.setFrameShadow(QFrame.Shadow.Sunken)
+        #linea2.setFrameShadow(QFrame.Shadow.Sunken)
         
         # ---
 
+        self.main_layout.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed))
+        self.main_layout.addWidget(self.imagen_label)
+        self.main_layout.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed))
+        
         self.main_layout.addWidget(self.username_label)
         self.main_layout.addWidget(self.username_input)
-        self.main_layout.addSpacing(20)
-        self.main_layout.addWidget(linea1)
+        self.main_layout.addSpacing(15)
+        #self.main_layout.addWidget(linea1)
         self.main_layout.addWidget(self.password_label)
-        self.main_layout.addWidget(self.password_input)
-        self.main_layout.addSpacing(20)
-        self.main_layout.addWidget(linea2)
-        self.main_layout.addSpacing(20)
+        self.main_layout.addLayout(self.layout_contrasenia)
+        #self.main_layout.addWidget(self.password_input) <-- En caso de sacar el layout_contrasenia.
+        self.main_layout.addSpacing(25)
+        #self.main_layout.addWidget(linea2)
+        #self.main_layout.addSpacing(20)
         self.main_layout.addWidget(self.login_button)
         self.main_layout.addWidget(self.register_button)
         self.main_layout.addWidget(self.boton_volver)
@@ -180,3 +258,32 @@ class IniciarSesion(QDialog):
         self.hide()
         self.crear_partida_window = CrearPartida(self)
         self.crear_partida_window.show()
+        
+    def alternar_modo_echo(self):
+        # Estado anterior antes de cambiarlo:
+        estado_anterior = self.password_input.echoMode()
+
+        if estado_anterior == QLineEdit.EchoMode.Password:
+            self.password_input.setEchoMode(QLineEdit.EchoMode.Normal) # <-- Modo normal.
+        else:
+            self.password_input.setEchoMode(QLineEdit.EchoMode.Password) # <-- Modo echo (oculto).
+        
+        def terminar_cambio_ojo():
+            if estado_anterior == QLineEdit.EchoMode.Password:
+                pixmap = QPixmap("imagenes/ui/ojo.png").scaled(25, 25)
+            else:
+                pixmap = QPixmap("imagenes/ui/ojo_cerrado.png").scaled(25, 25)
+            self.icono_label.setPixmap(pixmap)
+            
+        pixmap_ojo_medio = QPixmap("imagenes/ui/ojo_medio.png").scaled(25, 25)
+        self.icono_label.setPixmap(pixmap_ojo_medio)
+
+        QTimer.singleShot(150, terminar_cambio_ojo) # <-- 150 ms de transición.
+
+    def centrar_ventana(self):
+        """Método para centrar la ventana en el centro de la pantalla."""
+        forma_pantalla = QGuiApplication.primaryScreen().availableGeometry()
+        forma_ventana = self.frameGeometry()
+        centro_pantalla = forma_pantalla.center()
+        forma_ventana.moveCenter(centro_pantalla)
+        self.move(forma_ventana.topLeft())
