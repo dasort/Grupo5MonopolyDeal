79e0f16618e2fdc84087a7b36f8a763c0af61baf
FrancoGCardenas <franco_cardenas_@outlook.com>
2024-11-26 05:45:39 +0000

    Cambios a controlador

--- controladores/controlador_iniciar_sesion.py
+++ controladores/controlador_iniciar_sesion.py
@@ -1,14 +1,13 @@
-
 from vistas.vista_iniciar_sesion import IniciarSesion
 from vistas.vista_main_menu import MainMenu
 from controladores.controlador_crear_cuenta import Controlador_crear_cuenta
-from modelo.base_de_datos.jugador_dao.hash_contrasenia import hash_contrasenia
+from modelo.base_de_datos.jugador_dao.hash_contrasenia import hash_contrasenia, compara_contrasenia
 from PyQt6.QtWidgets import QMessageBox
 from modelo.base_de_datos.jugador_dao.jugador_bdd import JugadorBDD
 
 
 class Controlador_iniciar_sesion():
-    def __init__(self,main_menu):
+    def __init__(self, main_menu):
         self.main_menu = main_menu
         self._vista = IniciarSesion(self)  
         self._vista.show()
@@ -20,18 +19,19 @@ class Controlador_iniciar_sesion():
         contrasena_guardada = jugador.datos_bdd['contrasenia'] 
         salt = jugador.datos_bdd['salt'] 
         
-        if hash_contrasenia.compara_contrasenias(contrasena_ingresada, contrasena_guardada, salt): 
+        if compara_contrasenia(contrasena_ingresada, contrasena_guardada, salt): 
             self.show_info_dialog("Inicio de sesión exitoso.") 
         else: 
             jugador.datos_bdd = None 
             self.show_error_dialog("Contraseña incorrecta.")
-            
+
     def show_error_dialog(self, message): 
         msg_box = QMessageBox() 
         msg_box.setIcon(QMessageBox.Icon.Critical) 
         msg_box.setText(message) 
         msg_box.setWindowTitle("Error") 
-        msg_box.exec() 
+        msg_box.exec()
+        
     def show_info_dialog(self, message): 
         msg_box = QMessageBox() 
         msg_box.setIcon(QMessageBox.Icon.Information) 
@@ -39,19 +39,6 @@ class Controlador_iniciar_sesion():
         msg_box.setWindowTitle("Información") 
         msg_box.exec()   
         
-        
     def abrirSegunda(self):
         self._var = Controlador_crear_cuenta()
         self._vista.close()
-        
-        
-
-
-    
-    
-
-
-
-
-
-
