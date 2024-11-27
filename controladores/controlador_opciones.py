b272d6b82ca27c89dd4e570add680e2a3abd2b30
Lucas Bonifacio <lucasub360@gmail.com>
2024-11-26 23:27:39 -0300

    Agregué un botón para el volumen que funciona perfecto en el menú de opciones.
    Sirve para alternar entre apagado o máximo, cambia el ícono dinámicamente, todo funciona perfecto en eso.

--- controladores/controlador_opciones.py
+++ controladores/controlador_opciones.py
@@ -15,6 +15,15 @@ class ControladorOpciones:
     def cambiar_volumen(self, valor):
         volumen = valor / 100 # <-- Para convertirlo en un número flotante, el cual setVolume puede manejar (0.0 ~ 1.0).
         self.__main_menu.set_volumen(volumen)
+        self.__vista.cambiar_pixmap_volumen()
     
     def obtener_volumen(self):
         return int (self.__main_menu.get_volumen() * 100)
+    
+    def alternar_volumen(self):
+        if self.obtener_volumen() >= 50:
+            self.__vista.cambiar_icono("apagado")
+            self.cambiar_volumen(0)
+        else:
+            self.__vista.cambiar_icono("alto")
+            self.cambiar_volumen(100)
